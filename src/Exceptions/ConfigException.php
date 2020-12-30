6ec4cb47df0480c592b44817ed59aff3f6068d5b
lozovoyv <lozovoyv@gmail.com>
2020-12-30 18:19:45 +0300

    Initial

---
+++ src/Exceptions/ConfigException.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the OpxCore.
+ *
+ * Copyright (c) Lozovoy Vyacheslav <opxcore@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OpxCore\Config\Exceptions;
+
+use Exception;
+
+class ConfigException extends Exception
+{
+
+}
---
+++ src/Interfaces/ConfigInterface.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * This file is part of the OpxCore.
+ *
+ * Copyright (c) Lozovoy Vyacheslav <opxcore@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace OpxCore\Config\Interfaces;
+
+interface ConfigInterface
+{
+    /**
+     * Load configuration.
+     *
+     * @param string|null $profile
+     * @param bool $force
+     *
+     * @return  bool
+     */
+    public function load(?string $profile = null, bool $force = false): bool;
+
+    /**
+     * Determine if the given configuration value exists.
+     *
+     * @param string $key
+     *
+     * @return  bool
+     */
+    public function has(string $key): bool;
+
+    /**
+     * Set a given configuration value.
+     *
+     * @param array|string|null $key
+     * @param mixed|null $value
+     *
+     * @return  void
+     */
+    public function set($key, $value = null): void;
+
+    /**
+     * Get the specified configuration value.
+     *
+     * @param string $key
+     * @param mixed $default
+     *
+     * @return  mixed
+     */
+    public function get(string $key, $default = null);
+
+    /**
+     * Get all of the configuration items for the application.
+     *
+     * @return  array
+     */
+    public function all(): array;
+
+    /**
+     * Push a value onto an array configuration value.
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @return  void
+     */
+    public function push(string $key, $value): void;
+}
